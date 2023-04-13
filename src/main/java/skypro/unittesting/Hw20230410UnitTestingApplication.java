4e311b5a42de1e906e91787b083a1466922d6c76
Qwaieare <gb-grand@mail.ru>
2023-04-13 19:49:13 +0300

    initial commit for hw20230410_unitTesting

---
+++ src/main/java/skypro/unittesting/Hw20230410UnitTestingApplication.java
@@ -0,0 +1,13 @@
+package skypro.unittesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw20230410UnitTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw20230410UnitTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/skypro/unittesting/Hw20230410UnitTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.unittesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw20230410UnitTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
